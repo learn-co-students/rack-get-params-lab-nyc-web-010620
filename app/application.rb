f7868c87502fbe5099756e497cf29af51b7f18b0
github username <github email address>
2020-01-27 14:14:27 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,10 +16,27 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added Figs"
+      else 
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
-
+    # binding.pry
     resp.finish
   end
 
@@ -28,3 +48,11 @@ class Application
     end
   end
 end
+
+
+# ohh 
+# so you have a response and a request from this
+
+# search_term = req.params["item"]
+# this is acts a hash has a key and a value
+# '/add?item=Figs' <- from the rspec/ How it looks like on the browser
